7d82d622f9eb15327c303b647e5918e4678ba020
Jason Sexauer <genericcarbonlifeform@gmail.com>
2016-02-15 11:17:24 -0500

    Remove old rule implementation.  Add clip to bounds option.

--- skfuzzy/control/controlsystem.py
+++ skfuzzy/control/controlsystem.py
@@ -19,180 +19,6 @@ except ImportError:
     from .ordereddict import OrderedDict
 
 
-class OLD_Rule(object):
-    """
-    Define a new fuzzzy rule relating universe variables in memory.
-
-    Parameters
-    ----------
-    antecedents : Antecedent or iterable of Antecedents
-        Fuzzy antecedents (input/sensors) as from ``skfuzzy.Antecedent``.
-    consequents : Consequent or iterable of Consequents
-        Fuzzy consequents (output/control) as from ``skfuzzy.Consequent``.
-        Universe variable for the fuzzy consequent.
-    kind : string
-        Defines the relationship of antecedents. 'or' will combine with a
-        maximum operator, while 'and' will combine with a minimum operator.
-
-    Notes
-    -----
-    Boolean logic order of operations (NOT > AND > OR) is followed.
-    """
-
-    def __init__(self, antecedents=None, consequents=None, kind='or',
-                 label=None):
-        self._chk_kind(kind)
-        self.kind = kind.lower()
-        self.antecedents = self._chk_obj(antecedents, Antecedent)
-        self.consequents = self._chk_obj(consequents, Consequent)
-        self.label = label
-        self.graph = nx.DiGraph()
-        self.graph.add_node(self)
-        self.graph.node[self]['kind'] = kind
-        self._id = id(self)
-
-        # Debugging state
-        self.collected_firing = {}
-        self.final_firing = None
-
-        if antecedents is None or consequents is None:
-            self.connections = None
-        else:
-            self._connect(antecedents, consequents, kind)
-
-    def __repr__(self):
-        """
-        Print a concise, readable summary of the fuzzy rule.
-        """
-        if self.label is not None:
-            return self.label
-        # All this for a pretty printed representation of the rule...
-        antlen = len(self.antecedents)
-        conlen = len(self.consequents)
-
-        ant_str = ''
-        con_str = ''
-
-        for antecedent in self.antecedents:
-            ant_str += antecedent.full_label + ', '
-
-        for consequent in self.consequents:
-            con_str += consequent.full_label + ', '
-
-        ant_str = ant_str[:-2]
-        con_str = con_str[:-2]
-
-        if antlen > 1:
-            ant_str = '[' + ant_str + ']'
-
-        if conlen > 1:
-            con_str = '[' + con_str + ']'
-
-        return "{0}-Rule connecting {1} with {2}".format(
-            self.kind.upper(), ant_str, con_str)
-
-    def _chk_obj(self, var, obj):
-        """
-        Argument checking to ensure every adjective's parent is type ``obj``.
-        """
-        temp = self._iter(var)
-        for term in temp:
-            parent = term.parent_variable
-            if not isinstance(term, FuzzyVariableTerm):
-                raise ValueError("All elements must be terms")
-            if parent is None:
-                raise ValueError("All terms must have a parent")
-            if not isinstance(parent, obj):
-                raise ValueError("All term's variables must be of type "
-                                 "{0}".format(obj))
-        return temp
-
-    def _iter(self, var):
-        """
-        Ensure a variable is iterable; wrap in a list if necessary.
-        """
-        if issubclass(var.__class__, FuzzyVariableTerm):
-            return [var, ]
-        else:
-            return var
-
-    def _chk_kind(self, string):
-        """
-        The only accepted kinds of fuzzy rules are 'or', or 'and'.
-        """
-        string = string.lower()
-        if string == 'or' or string == 'and':
-            return
-        else:
-            raise ValueError("Incorrect kind. Options are 'or' or 'and'.")
-
-    def add_antecedent(self, antecedent_term):
-        """
-        Populate the graph with a new antecedent, connecting it to this rule.
-        """
-        assert isinstance(antecedent_term, FuzzyVariableTerm)
-        antecedent = antecedent_term.parent_variable
-        assert isinstance(antecedent, Antecedent)
-
-        # Antecedent -> Antecedent_Term -> Rule
-        self.graph.add_path([antecedent, antecedent_term])
-        self.graph.add_path([antecedent_term, self])
-
-
-    def add_consequent(self, consequent_term):
-        """
-        Populate the graph with a new consequent, connecting it to this rule.
-        """
-        assert isinstance(consequent_term, FuzzyVariableTerm)
-        consequent = consequent_term.parent_variable
-        assert isinstance(consequent, Consequent)
-
-        # Rule -> Consequent_Term -> Consequent
-        self.graph.add_path([self, consequent_term])
-        self.graph.add_path([consequent_term, consequent])
-
-
-
-    def _connect(self, antecedents=None, consequents=None, kind='or'):
-        """
-        Private method used when arguments provided on rule instantiation.
-        """
-        for antecedent in self.antecedents:
-            self.add_antecedent(antecedent)
-
-        for consequent in self.consequents:
-            self.add_consequent(consequent)
-
-    def compute(self):
-        """
-        Execute this fuzzy rule.
-        """
-
-        # Collect the firing of all input membership functions
-        self.collected_firing = {}
-        for antecedent_term in self.graph.predecessors(self):
-            mv = antecedent_term.membership_value
-            if mv is None:
-                raise Exception("Membership value missing for " +
-                                antecedent_term.full_label)
-            self.collected_firing[antecedent_term.full_label] = mv
-
-        # Combine membership function firing as appropriate for this rule
-        if self.kind == 'or':
-            self.final_firing = np.asarray(self.collected_firing.values()).max()
-        elif self.kind == 'and':
-            self.final_firing = np.asarray(self.collected_firing.values()).min()
-        else:
-            raise NotImplementedError("Unexpected kind: " + self.kind)
-
-        # Cap output membership function(s) in consequents
-        for consequent_term in self.graph.successors(self):
-            consequent_term.parent_variable.set_patch(
-                consequent_term.label, self.final_firing)
-
-    def view(self):
-        ControlSystemVisualizer(self).view().show()
-
 class ControlSystem(object):
     """
     Fuzzy Control System
@@ -263,17 +89,34 @@ class _InputAcceptor(object):
         if len(matches) == 0:
             raise ValueError("Unexpected input: " + key)
         assert len(matches) == 1
-        matches[0].input[self.sim] = value
+        var = matches[0]
+
+        if value > max(var.universe):
+            if self.sim.clip_to_bounds:
+                value = max(var.universe)
+            else:
+                raise ValueError("Value is out of bounds.  Max is %s" %
+                                 max(var.universe))
+        if value < min(var.universe):
+            if self.sim.clip_to_bounds:
+                value = min(var.universe)
+            else:
+                raise ValueError("Value is out of bounds.  Min is %s" %
+                                 min(var.universe))
+
+        var.input[self.sim] = value
 
 
 class ControlSystemSimulation(object):
 
-    def __init__(self, control_system):
+    def __init__(self, control_system, clip_to_bounds = False):
         assert isinstance(control_system, ControlSystem)
         self.ctrl = control_system
 
         self.input = _InputAcceptor(self)
         self.output = OrderedDict()
+
+        self.clip_to_bounds = clip_to_bounds
 
     def inputs(self, input_dict):
         """
