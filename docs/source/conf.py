b65665d6315bc6b6c71ed5896d0f40a82de66463
Josh Warner <silvertrumpet999+github@gmail.com>
2016-05-07 09:57:09 -0500

    DOCFIX: Import skfuzzy after updating sys.path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@
 
 import sys
 import os
-import skfuzzy
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -27,6 +26,7 @@ src_dir = os.path.abspath(os.path.join(curpath, '..', '..'))
 sys.path.insert(0, src_dir)
 
 module = 'skfuzzy'
+import skfuzzy
 
 # -- General configuration ------------------------------------------------
 
