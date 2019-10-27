1cff29e6ff31274d62f28e451dd208a55c301f0e
Wouter Van den Broeck <wouter.vandenbroeck@iminds.be>
2019-10-27 11:27:54 +0100

    fix: Pillow 6.0.0 and above have removed the 'VERSION' attribute.

--- tools/build_versions.py
+++ tools/build_versions.py
@@ -11,6 +11,12 @@ import networkx
 for m in (np, sp, Image, six, networkx):
     if not m is None:
         if m is Image:
-            print('PIL'.rjust(10), ' ', Image.VERSION)
+            # Pillow 6.0.0 and above have removed the 'VERSION' attribute
+            # https://bitbucket.org/rptlab/reportlab/issues/176/incompatibility-with-pillow-600
+            try:
+                im_ver = Image.__version__
+            except AttributeError:
+                im_ver = Image.VERSION
+            print('PIL'.rjust(10), ' ', im_ver)
         else:
             print(m.__name__.rjust(10), ' ', m.__version__)
