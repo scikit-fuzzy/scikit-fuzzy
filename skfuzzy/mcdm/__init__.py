2c7610bb1af730006e90a9304c075dc8ebfd9bcb
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2022-08-04 17:36:38 +0100

    just starting the new module for MCDM

---
+++ skfuzzy/mcdm/__init__.py
@@ -0,0 +1,10 @@
+"""
+Fuzzy Multiple-criteria decision-making (MCDM) subpackage,
+containing fuzzy TOPSIS MCDM algorithm.
+
+"""
+# __all__ = ['cmeans',
+#            'cmeans_predict',
+#            ]
+
+# from ._cmeans import cmeans, cmeans_predict
